ba0fd45b1984946717828a0ac65e2bf692c6f7c4
Mesi14 <Mesi18@proton.me>
2025-02-13 13:22:19 +0200

    :add core functionality to the details page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,9 +1,20 @@
 import "../styles/Details.css"
+import { Button, Nav } from "react-bootstrap";
 
 function Details() {
-  return (
-    <div className="details">
-      <h1>Basic info about the app</h1>
+  return (  
+    <div className="details-descript">
+      <img
+        alt="cabs in the city"
+        src={require("../assets/cabs.jpg")}
+      />
+      <div>
+        <h3>Title</h3>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        <Button variant="primary">
+          <Nav.Link href="/">Go back</Nav.Link>
+        </Button>
+      </div>
     </div>
   );
 }
--- src/components/Photos.js
+++ src/components/Photos.js
@@ -1,9 +1,73 @@
+import {Container, Row, Col, Card, Button, Nav} from 'react-bootstrap';
+
 const Photos = () => {
  return (
-  <div className="photos">
-    <h3>dsfjghj </h3>
-  </div>
+  <Container>
+    <Row>
+      <Col>
+        <Card className="mt-5" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={require("../assets/onion.jpg")} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">
+              <Nav.Link href="/details/:id">Photo Id</Nav.Link>
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="mt-5" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={require("../assets/cabs.jpg")} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">
+              <Nav.Link href="/details/:id">Photo Id</Nav.Link>
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col>
+        <Card className="mt-5" style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={require("../assets/computer.jpg")} />
+          <Card.Body>
+            <Card.Title>Card Title</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+            <Button variant="primary">
+              <Nav.Link href="/details/:id">Photo Id</Nav.Link>
+            </Button>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
 );
 }
 
 export default Photos;
+
+// function ResponsiveAutoExample() {
+//   return (
+//     <Container>
+//       <Row>
+//         <Col={8}>sm=8</Col=>
+//         <Col sm={4}>sm=4</Col>
+//       </Row>
+//       <Row>
+//         <Col sm>sm=true</Col>
+//         <Col sm>sm=true</Col>
+//         <Col sm>sm=true</Col>
+//       </Row>
+//     </Container>
+//   );
+// }
