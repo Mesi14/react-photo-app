a0d1753b87c9f60a682ce296c9c3e77e6f725244
rusumvlad <vlad.rusu@student.upt.ro>
2025-02-16 23:29:27 +0200

    Fixed Photo details Page

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,34 +1,32 @@
 import { useParams } from "react-router";
-import "../styles/Details.css"
+import "../styles/Details.css";
 import { Button, Nav } from "react-bootstrap";
 import { useState, useEffect } from "react";
 import axios from "axios";
-
 
 function Details() {
   const parameters = useParams();
   const [photo, setPhoto] = useState({});
 
-  useEffect( () => {
+  useEffect(() => {
     const fetchData = async () => {
       try {
-        const resp = await axios.get(`http://localhost:3001/photos/${parameters.imgId}`);
-        setPhoto(resp.data)
+        const resp = await axios.get(
+          `http://localhost:3001/photos/${parameters.imgId}`
+        );
+        setPhoto(resp.data);
       } catch (error) {
-        console.log("this error is from single photo page", error)
+        console.log("this error is from single photo page", error);
       }
-    }
+    };
 
     window.setTimeout(() => fetchData(), 1000);
   }, []);
-  console.log(photo)
+  console.log(photo);
 
-  return (  
+  return (
     <div className="details-descript">
-      <img
-        alt="cabs in the city"
-        src={`${photo.srcImage}`}
-      />
+      <img alt={photo.title} src={`/${photo.srcImage}`} />
       <div>
         <h3>{photo.title}</h3>
         <p>{photo.desc}</p>
